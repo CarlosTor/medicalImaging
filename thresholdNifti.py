f0095aaa24fd37ece2713763f9765485303632f6
CarlosTor <carlostordiez@gmail.com>
2019-09-17 14:15:20 +0200

    Correcting the multi-thresholding process

--- thresholdNifti.py
+++ thresholdNifti.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     parser.add_argument('-i', '--input', help='Input image (required, accepted multiple times)', type=str, nargs='*', required = True)
     parser.add_argument('-t', '--threshold', help='Threshold value (default Otsu method)', type=float, nargs='*', default=[-1], required = False)
-    parser.add_argument('-o', '--output', help='Prefix added to input name for output image (default \'_threshold\' + threshold_value)', type=str, default='_threshold', required = False)
+    parser.add_argument('-o', '--output', help='Prefix added to input name for output image (default \'_th\' + threshold_value)', type=str, default='_th', required = False)
 
 
     args = parser.parse_args()
@@ -58,16 +58,6 @@ if __name__ == '__main__':
             thresholds = args.threshold[0]*np.ones(len(input))
 
 
-
-    for th in thresholds:
-        if th<0:
-            thresholds=list()
-            for im in input:
-                thresholds.append(filters.threshold_otsu(im))
-        else:
-            thresholds.append(th)
-
-
     for i,im in enumerate(input):
         im[im<thresholds[i]]=0
         im[im>=thresholds[i]]=1
@@ -79,4 +69,4 @@ if __name__ == '__main__':
         else:
             print('Input file must be a nifti image')
             sys.exit()
-        nibabel.save(nibabel.Nifti1Image(im,inputImage.affine),inputName+args.output+'0'+str(int(round(thresholds[i]*1000)))+'.nii.gz')
+        nibabel.save(nibabel.Nifti1Image(im,inputImage.affine),inputName+args.output+str(thresholds[i])+'.nii.gz')
